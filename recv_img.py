a9d81c83afbdc378cfa920518f97a51e22fb89f9
Chen Haowei <chenhw26@qq.com>
2021-05-15 10:20:12 +0800

    test transfer latency

--- recv_img.py
+++ recv_img.py
@@ -1,17 +1,27 @@
 import zmq
+import time
 
 if __name__ == "__main__":
     context = zmq.Context()
 
     skt_recv_img = context.socket(zmq.SUB)
-    skt_recv_img.connect("tcp://127.0.0.1:55556")  # 远程ip
+    skt_recv_img.connect("tcp://127.0.0.1:5556")  # 远程ip
     skt_recv_img.set(zmq.SUBSCRIBE, b"")
 
-    skt_send_ack = context.socket(zmq.PUB)
-    skt_send_ack.bind("tcp://127.0.0.1:55555") # 本机ip
+    skt_warmup = context.socket(zmq.SUB)
+    skt_warmup.connect("tcp://127.0.0.1:5557")  # 远程ip
+    skt_warmup.set(zmq.SUBSCRIBE, b"")
 
-    skt_send_ack.send("begin".encode("utf-8"))
-    for i in range(100):
-        print("recv {} pic".format(i))
-        _ = skt_recv_img.recv()
-    skt_send_ack.send("ok".encode("utf-8"))
+    skt_send_ack = context.socket(zmq.PUB)
+    skt_send_ack.bind("tcp://127.0.0.1:5555") # 本机ip
+
+    _ = skt_warmup.recv()
+    for _ in range(1):
+        for i in range(100):
+            _ = skt_recv_img.recv()
+            time.sleep(0.1)
+            print("recv {} pic".format(i))
+        time.sleep(0.01)
+        for _ in range(10):
+            time.sleep(0.05)
+            skt_send_ack.send("ok".encode("utf-8"), flags=zmq.NOBLOCK)
--- send_img.py
+++ send_img.py
@@ -7,23 +7,28 @@ import cv2
 if __name__ == "__main__":
     context = zmq.Context()
     skt_send_img = context.socket(zmq.PUB)
-    skt_send_img.bind("tcp://127.0.0.1:55556")  # 本机ip
+    skt_send_img.bind("tcp://127.0.0.1:5556")  # 本机ip
+
+    skt_warmup = context.socket(zmq.PUB)
+    skt_warmup.bind("tcp://127.0.0.1:5557")  # 本机ip
+    for _ in range(10):
+        time.sleep(0.05)
+        skt_warmup.send("ss".encode("utf-8"))
 
     skt_recv_ack = context.socket(zmq.SUB)
-    skt_recv_ack.connect("tcp://127.0.0.1:55555")  # 远程ip
-    skt_recv_ack.set(zmq.SUBSCRIBE, "".encode("utf-8"))
+    skt_recv_ack.connect("tcp://127.0.0.1:5555")  # 远程ip
+    skt_recv_ack.set(zmq.SUBSCRIBE, b"")
 
-    print("Wait for ack")
-    _ = skt_recv_ack.recv()
-    for res in [112, 224, 448]:
+    for res in [112]:
         print("Res: {}".format(res))
         img = load_img("test_img.jpg", crop_size=(448, 448), target_size=(res, res))
-        img_encode = cv2.imencode(".jpg", img)[1]
+        img_encode = cv2.imencode(".jpg", np.array(img))[1]
         data = np.array(img_encode).tostring()
         start = time.time()
         for i in range(100):
             print("send {} pic".format(i))
-            skt_send_img.send(data, zmq.NOBLOCK)
+            time.sleep(0.01)
+            skt_send_img.send(data)
         _ = skt_recv_ack.recv()
         end = time.time()
         print("Latency per img: {:.5f}s".format((end-start)/100.0))
