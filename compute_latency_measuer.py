7bbae00f9f6e147955b091025074b5afbdc932e9
Chen Haowei <chenhw26@qq.com>
2021-05-26 19:17:12 +0800

    delete pic

--- compute_latency_measuer.py
+++ compute_latency_measuer.py
@@ -2,28 +2,26 @@ import torch
 import torchvision
 import spp_model_small
 import time
-from image_utils import load_img
 
 if __name__ == "__main__":
     transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
 
     # cnn_ori = spp_model.MycnnSPPNetOri().cpu()
     # cnn_ori.load_state_dict(torch.load("saved_models/cnn_spp.pt", map_location=torch.device("cpu")))
-    cnn_ori = spp_model_small.MycnnSPPNetOri().cpu()
+    cnn_ori = spp_model_small.MycnnSPPNetOri().cuda()
     cnn_ori.eval()
 
-    bottlenet = spp_model_small.MycnnBottlenetDronePart(2).cpu()
-    bottlenet.eval()
+    # bottlenet = spp_model_small.MycnnBottlenetDronePart(2).cpu()
+    # bottlenet.eval()
 
     print("Local compute latency:")
     for res in [112, 224, 448]:
-        img = transform(load_img("test_img.jpg", crop_size=(448, 448), target_size=(res, res)))
-        img = torch.unsqueeze(img, 0)
+        img = torch.rand(1, 3, res, res)
         start = time.time()
         for _ in range(100):
             _ = cnn_ori(img)
         end = time.time()
-        print("Res {}: {:.2f}ms/img".format(res, (end-start)*10))
+        print("Res {}: {:.5f}s/img".format(res, (end-start)/100))
 
     # print("\nBottlenet compute latency:")
     # for res in [112, 224, 448]:
@@ -35,16 +33,16 @@ if __name__ == "__main__":
     #     end = time.time()
     #     print("Res {}: {:.2f}ms/img".format(res, (end-start)*10))
 
-    bottlenet_server = spp_model_small.MycnnBottlenetServerPart(2)
-    bottlenet_server.eval()
-
-    print("\nServer bottlenet compute latency:")
-    for res in [112, 224, 448]:
-        img = transform(load_img("test_img.jpg", crop_size=(448, 448), target_size=(res, res)))
-        img = torch.unsqueeze(img, 0)
-        inputs = bottlenet(img)
-        start = time.time()
-        for _ in range(100):
-            _ = bottlenet_server(inputs)
-        end = time.time()
-        print("Res {}: {:.2f}ms/img".format(res, (end-start)*10))
+    # bottlenet_server = spp_model_small.MycnnBottlenetServerPart(2)
+    # bottlenet_server.eval()
+    #
+    # print("\nServer bottlenet compute latency:")
+    # for res in [112, 224, 448]:
+    #     img = transform(load_img("test_img.jpg", crop_size=(448, 448), target_size=(res, res)))
+    #     img = torch.unsqueeze(img, 0)
+    #     inputs = bottlenet(img)
+    #     start = time.time()
+    #     for _ in range(100):
+    #         _ = bottlenet_server(inputs)
+    #     end = time.time()
+    #     print("Res {}: {:.2f}ms/img".format(res, (end-start)*10))
