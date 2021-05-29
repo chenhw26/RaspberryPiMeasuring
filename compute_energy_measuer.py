bd3b8cba8f298220a98f72a12fd77ce156712cd4
Chen Haowei <chenhw26@qq.com>
2021-05-29 15:14:33 +0800

    togpu

--- compute_energy_measuer.py
+++ compute_energy_measuer.py
@@ -3,19 +3,19 @@ import spp_model_small
 import time
 
 if __name__ == "__main__":
-    cnn_ori = spp_model_small.MycnnSPPNetOri()
+    cnn_ori = spp_model_small.MycnnSPPNetOri().cuda()
     cnn_ori.eval()
 
     for res in [112, 224, 448]:
-        img = torch.rand(1, 3, res, res)
+        img = torch.rand(1, 3, res, res).cuda()
         for _ in range(10):
             _ = cnn_ori(img)
 
     print("Local compute latency:")
     res = 448
-    img = torch.rand(1, 3, res, res)
+    img = torch.rand(1, 3, res, res).cuda()
     while True:
-        timer = time.time()
+        # timer = time.time()
         _ = cnn_ori(img)
-        timer_end = time.time()
-        if timer_end-timer < 0.2: time.sleep(0.2-timer_end+timer)
+        # timer_end = time.time()
+        # if timer_end-timer < 0.2: time.sleep(0.2-timer_end+timer)
