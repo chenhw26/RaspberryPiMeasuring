d8ca62ed27074eec538c9b241bab4ce45110a775
Chen Haowei <chenhw26@qq.com>
2021-05-29 11:17:00 +0800

    compute energy

---
+++ compute_energy_measuer.py
@@ -0,0 +1,21 @@
+import torch
+import spp_model_small
+import time
+
+if __name__ == "__main__":
+    cnn_ori = spp_model_small.MycnnSPPNetOri()
+    cnn_ori.eval()
+
+    for res in [112, 224, 448]:
+        img = torch.rand(1, 3, res, res)
+        for _ in range(10):
+            _ = cnn_ori(img)
+
+    print("Local compute latency:")
+    res = 112
+    img = torch.rand(1, 3, res, res)
+    while True:
+        timer = time.time()
+        _ = cnn_ori(img)
+        timer_end = time.time()
+        if timer_end-timer < 0.2: time.sleep(0.2-timer_end+timer)
--- compute_latency_measuer.py
+++ compute_latency_measuer.py
@@ -21,10 +21,7 @@ if __name__ == "__main__":
         img = torch.rand(1, 3, res, res)
         start = time.time()
         for _ in range(50):
-            timer = time.time()
             _ = cnn_ori(img)
-            timer_end = time.time()
-            if timer_end-timer < 0.2: time.sleep(0.2-timer_end+timer)
         end = time.time()
         print("Res {}: {:.5f}s/img".format(res, (end-start)/50))
 
